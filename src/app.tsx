bdc6e1f1de4fc43dad5b14e4f49f141a473c3431
Kyle <lauber.kyle@gmail.com>
2024-06-21 16:10:29 -0700

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,17 @@
+import '@unocss/reset/tailwind.css'
+import 'virtual:uno.css'
+
+export default function App() {
+  return (
+    <Router
+      root={props => (
+        <>
+          <Nav />
+          <Suspense>{props.children}</Suspense>
+        </>
+      )}
+    >
+      <FileRoutes />
+    </Router>
+  )
+}
