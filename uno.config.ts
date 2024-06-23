312ab561f655deaef46bc08fa3df3d0f0d77558d
Kyle <lauber.kyle@gmail.com>
2024-06-23 04:11:26 -0700

    chore: unocss-shadcn

--- uno.config.ts
+++ uno.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig, presetUno } from 'unocss'
+import presetAnimations from 'unocss-preset-animations'
+import { presetShadcn } from 'unocss-preset-shadcn'
 
 export default defineConfig({
   presets: [
     presetUno(),
+    presetAnimations(),
+    presetShadcn(),
   ],
 })
