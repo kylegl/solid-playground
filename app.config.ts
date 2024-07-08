33f406d9b94357a1956ed4168ef0684352614ee4
Kyle <lauber.kyle@gmail.com>
2024-07-08 02:54:51 -0700

    chore: install shadcn-solid

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,11 @@ import { defineConfig } from '@solidjs/start/config'
 import UnoCSS from 'unocss/vite'
 import devtools from 'solid-devtools/vite'
 
+// TODO: look into getting unocss-preset-shadcn to work with kobalte. currently it uses radix which is similar to kobalte. might just need to swap
+// some var names
+
+// uhh whats this? https://github.com/hngngn/shadcn-solid
+
 export default defineConfig({
   vite: {
     plugins: [
